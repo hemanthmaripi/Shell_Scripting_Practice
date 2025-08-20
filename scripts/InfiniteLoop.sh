433e3ef0e011864d30a3f01a53483a608f2aba1d
Hemanth Maripi <hmaripi6@gmail.com>
2025-08-20 11:20:54 +0000

    Added Infinite Loop

---
+++ scripts/InfiniteLoop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Infinite loops are used to run a particular task continously 24/7 like monitoring things and all
+
+#this an be achieved by using while loop by giving the condition true
+
+while true
+do
+	echo "Infinite loop running"
+	sleep 2s    #sleeping the terminal for 2secs
+done
+
